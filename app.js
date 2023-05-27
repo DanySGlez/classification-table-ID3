b660226673f3112d24c90ebf90bea79581118cec
Daniel Salomón <66494746+DanySGlez@users.noreply.github.com>
2023-05-26 21:22:48 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+var daniel = {
+  nome: "Daniel",
+  vitorias: 0,
+  empates: 0,
+  derrotas: 0,
+  pontos: 0
+};
+var carlos = {
+  nome: "Carlos",
+  vitorias: 0,
+  empates: 0,
+  derrotas: 0,
+  pontos: 0
+};
+var juan = {
+  nome: "Juan",
+  vitorias: 0,
+  empates: 0,
+  derrotas: 0,
+  pontos: 0
+};
+
+function calculaPontos(jogador) {
+  var pontos = jogador.vitorias * 3 + jogador.empates;
+  return pontos;
+}
+
+daniel.pontos = calculaPontos(daniel);
+carlos.pontos = calculaPontos(carlos);
+carlos.pontos = calculaPontos(juan);
+
+var jogadores = [daniel, carlos, juan];
+
+console.log(jogadores);
+
+function exibeJogadoresNaTela(jogadores) {
+  var elemento = "";
+  for (var i = 0; i < jogadores.length; i++) {
+    elemento += "<tr><td>" + jogadores[i].nome + "</td>";
+    elemento += "<td>" + jogadores[i].vitorias + "</td>";
+    elemento += "<td>" + jogadores[i].empates + "</td>";
+    elemento += "<td>" + jogadores[i].derrotas + "</td>";
+    elemento += "<td>" + jogadores[i].pontos + "</td>";
+    elemento +=
+      "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>";
+    elemento += "</tr>";
+  }
+
+  var tabelaJogadores = document.getElementById("tabelaJogadores");
+  tabelaJogadores.innerHTML = elemento;
+}
+
+exibeJogadoresNaTela(jogadores);
+
+function adicionarVitoria(i) {
+  var jogador = jogadores[i];
+  jogador.vitorias++;
+  jogador.pontos = calculaPontos(jogador);
+  exibeJogadoresNaTela(jogadores);
+}
+
+function adicionarEmpate(i) {
+  var jogador = jogadores[i];
+  jogador.empates++;
+  jogador.pontos = calculaPontos(jogador);
+  exibeJogadoresNaTela(jogadores);
+}
+
+function adicionarDerrota(i) {
+  var jogador = jogadores[i];
+  jogador.derrotas++;
+  exibeJogadoresNaTela(jogadores);
+}
